6d31a8c1f6045f651ad600c1f1f5127c12dcb8e2
Adele McKenna <theotheradele@gmail.com>
2023-07-24 23:42:07 -0400

    Switched API

--- src/app.js
+++ src/app.js
@@ -1,31 +1,22 @@
-let currentTime = new Date ();
+let currentTime = new Date();
 let day = currentTime.getDay();
 let hour = currentTime.getHours();
-if (hour<10) {
-  hour = `0${hour}`;}
+if (hour < 10) {
+  hour = `0${hour}`;
+}
 let minutes = currentTime.getMinutes();
-if (minutes<10) {
+if (minutes < 10) {
   minutes = `0${minutes}`;
 }
-let days = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
 let dayName = days[currentTime.getDay()];
+let today = document.querySelector("#today");
 today.innerHTML = `${dayName} ${hour}:${minutes}`;
-
-function searchCity(event) {
-event.preventDefault();
-  let searchInput=document.querySelector("#searchInput");
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = `${searchInput.value}`;
-  let apiKey = "5ef4de8cd6b7fefcd7c42f98cf464ce8";
-let city =searchInput.value;
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(function(response){
- let temperature = Math.round(response.data.main.temp);
-  let currentTemp = document.querySelector("#temperature");
-  currentTemp.innerHTML = `${temperature}`;
-});
-} 
-let searchForm = document.querySelector("form");
-searchForm.addEventListener("submit", searchCity);
-
