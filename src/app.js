ed8ac5374739e936831aeef586dcb39a362b82d0
Adele McKenna <theotheradele@gmail.com>
2023-07-27 00:37:55 -0400

    Fixed API call and added weather elements

--- src/app.js
+++ src/app.js
@@ -22,15 +22,21 @@ let today = document.querySelector("#today");
 today.innerHTML = `${dayName} ${hour}:${minutes}`;
 
 function searchCity(event) {
-event.preventDefault();
+  event.preventDefault();
   let searchInput=document.querySelector("#searchInput");
   let h1 = document.querySelector("h1");
   h1.innerHTML = `${searchInput.value}`;
   let city =searchInput.value;
   let apiKey = "0ocfta5c0e4602a2a90c32a9a4bbf5b9";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-console.log(apiUrl);
+  axios.get(apiUrl).then(function(response){
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    let humidityElement = document.querySelector("#humid");
+    humidityElement.innerHTML = response.data.temperature.humidity;
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = response.data.wind.speed;
+  });
 }
 let searchForm = document.querySelector("form");
 searchForm.addEventListener("submit", searchCity);
-console.log(apiUrl);
