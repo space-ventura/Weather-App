d746e7d58454c2bb6ba6ae82146ace2029077263
Adele McKenna <theotheradele@gmail.com>
2023-07-29 14:38:28 -0400

    Added unit conversion links

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@ today.innerHTML = `${dayName} ${hour}:${minutes}`;
 
 function searchCity(event) {
 event.preventDefault();
+
   let searchInput=document.querySelector("#searchInput");
   let h1 = document.querySelector("h1");
   h1.innerHTML = `${searchInput.value}`;
@@ -32,8 +33,9 @@ event.preventDefault();
   let apiKey = "0ocfta5c0e4602a2a90c32a9a4bbf5b9";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(function(response){
+    celsiusTemperature = Math.round(response.data.temperature.current);
     let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
     let humidityElement = document.querySelector("#humid");
     humidityElement.innerHTML = response.data.temperature.humidity;
     let windElement = document.querySelector("#wind");
@@ -47,5 +49,29 @@ event.preventDefault();
 
 let searchForm = document.querySelector("form");
 searchForm.addEventListener("submit", searchCity);
+
+function displayFahrenheit(event) {
+  event.preventDefault();
+  let fahrenheitTemperature = (celsiusTemperature * 9) /5 + 32;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click" , displayFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click" , displayCelsius);
+
 searchCity("Philadelphia");
+
+
 
